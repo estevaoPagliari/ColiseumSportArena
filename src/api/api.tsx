367698998042bc8b975fd0f24af3fb4337358047
estevaoPagliari <estevao_pagliari@hotmail.com>
2024-08-01 23:35:37 -0300

    atualizacao03

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  // baseURL: 'https://nexagendaserve-production.up.railway.app',
-  baseURL: 'http://192.168.1.105:8080',
+  baseURL: 'https://nexagendaserve-production.up.railway.app',
+  // baseURL: 'http://192.168.1.105:8080',
   // baseURL: 'http://192.168.3.24:8080',
 })
